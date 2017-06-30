d05dcb0356233350ee64172f9ced346a9758d2ff
Gergo Nagy <guzmop@gmail.com>
2017-06-30 16:31:48 +0100

    Pet shop tested

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -14,3 +14,45 @@ def pets_sold(pets)
   return pets[:admin][:pets_sold]
 end
 
+def increase_pets_sold(sold_pets, sold_pets_number)
+  return sold_pets[:admin][:pets_sold] += sold_pets_number
+end
+
+def stock_count(stock)
+  return stock[:pets].count 
+end
+
+def pets_by_breed(pet_shop, breed)
+  found_pets = []
+  for pet in pet_shop[:pets]
+    found_pets << pet if pet[:breed] == breed
+  end
+  return found_pets
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, remove_pet)
+  for pet in pet_shop[:pets]
+    pet.delete_if{|pet| remove_pet == remove_pet}
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets] << new_pet
+end
+
+def customer_pet_count(customers_pets)
+  return customers_pets[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+  return customer[:pets] << new_pet
+end
